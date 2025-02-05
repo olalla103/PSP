20634219ad02cfefc6d308d99b234e8c2ab1ef28
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2025-02-05 23:30:19 +0100

    si

--- Hilos/src/main/hilosActividades/Act2_3/HiloContador.java
+++
@@ -1,8 +0,0 @@
-package main.hilosActividades.Act2_3;
-
-public class HiloContador extends Thread {
-    private int contador;
-    public HiloContador(int contador) {
-        this.contador = contador;
-    }
-}
